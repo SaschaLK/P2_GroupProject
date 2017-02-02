b2ff02de343c80a534245031c958ffa160e6c442
Henry <rexyhn3@googlemail.com>
2017-02-02 14:33:42 +0100

    wawaw

--- RythmGame/src/game/MainController.java
+++ RythmGame/src/game/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 	private Timer timer;
 //	private Notes note=new Notes();
 	private NotesList list = new NotesList();
-	
+	private int time=0;
 	private Hitbox hitbox = new Hitbox();
 	private int score = 0;
 	public MainController(MyJFrame view){
@@ -34,30 +34,56 @@ public class MainController {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if(e.getKeyCode() == KeyEvent.VK_D){
-//		    		if(hitbox.hit0(note.getNote())){
-//		    			score =+ 10;
-//		    			view.setScore(score);
-//		    		}
-					for(Notes note : list.getNotesList()){
-						if(hitbox.hit0(note.getNote())){
-			    			score =+ 10;
-			    			view.setScore(score);
-			    		}
-					}
-		    	}				
+					checkh0();				
+		    	}
+				else if(e.getKeyCode() == KeyEvent.VK_F){
+					checkh1();				
+		    	}	
+				else if(e.getKeyCode() == KeyEvent.VK_J){
+					checkh2();				
+		    	}	
+				else if(e.getKeyCode() == KeyEvent.VK_K){
+					checkh3();				
+		    	}	
 			}
 		});
 		
 		timer.start();
 	}
 	private void update(){
+		time++;
+		list.checkTime(time);
 		list.moveNotes();
 		view.updateView(list.getNotesList());
 		
 	}
-	private void checkh1(){
+	private void checkh0(){
 		for(Notes note : list.getNotesList()){
 			if(hitbox.hit0(note.getNote())){
+    			score =+ 10;
+    			view.setScore(score);
+    		}
+		}
+	}
+	private void checkh1(){
+		for(Notes note : list.getNotesList()){
+			if(hitbox.hit1(note.getNote())){
+    			score =+ 10;
+    			view.setScore(score);
+    		}
+		}
+	}
+	private void checkh2(){
+		for(Notes note : list.getNotesList()){
+			if(hitbox.hit2(note.getNote())){
+    			score =+ 10;
+    			view.setScore(score);
+    		}
+		}
+	}
+	private void checkh3(){
+		for(Notes note : list.getNotesList()){
+			if(hitbox.hit3(note.getNote())){
     			score =+ 10;
     			view.setScore(score);
     		}
--- RythmGame/src/game/NotesList.java
+++ RythmGame/src/game/NotesList.java
@@ -8,9 +8,9 @@ public class NotesList {
 	
 	public NotesList(){
 		list.add(new Notes(0));
-		list.add(new Notes(1));
-		list.add(new Notes(2));
-		list.add(new Notes(3));
+//		list.add(new Notes(1));
+//		list.add(new Notes(2));
+//		list.add(new Notes(3));
 	}
 	public void moveNotes(){
 		for(Notes notes :list){
@@ -23,5 +23,17 @@ public class NotesList {
 	}
 	public void setList(ArrayList<Notes> nlist){
 		list=nlist;
+	}
+	public void checkTime(int i){
+		if(i==100){
+			list.add(new Notes(1));
+		}
+		else if(i==200){
+			list.add(new Notes(3));
+		}
+		else if(i==300){
+			list.add(new Notes(2));
+		}
+		
 	}
 }
