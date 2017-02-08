f23541b4e708b87de0eaa6b77420ebd55621b5e8
SaschaLK <SaschaL.Kufahl@gmail.com>
2017-02-08 18:19:40 +0100

    Added SongSelect
    Auswahl der Songs über JDialog.
    Custom ActionListener für Buttons

--- RythmGame/src/game/MainController.java
+++ RythmGame/src/game/MainController.java
@@ -2,6 +2,7 @@
 package game;
 
 import java.awt.Color;
+import java.awt.Frame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,41 +19,54 @@ public class MainController {
 	private Timer timer;
 
 	private NotesList list = new NotesList();
-	private int time=0;
+	private int time = 0;
 	private Hitbox hitbox = new Hitbox();
 	private int score = 0;
 
-	private int color=0;
-	private Thread t1=new Thread();
-	private Thread t2=new Thread();
-	private Thread t3=new Thread();
-	private Thread t4=new Thread();
+	private int color = 0;
+	private Thread t1 = new Thread();
+	private Thread t2 = new Thread();
+	private Thread t3 = new Thread();
+	private Thread t4 = new Thread();
 	
+	private String file;
+
 	private SongSelectDialog ssDialog;
-	
-	public MainController(MyJFrame view){
-		
-		//JDialog test = new J
-				
+
+	public MainController(MyJFrame view) {
+
 		this.view = view;
-		timer = new Timer(5,listener -> update());
-		view.getStart().addActionListener(listener->{
-				//ready();
-				//playSound();
-				selectSong();
-				});
-		//Sollte in der Lage sein 2 noten zuerfassen Thread
+		timer = new Timer(5, listener -> update());
+		view.getStart().addActionListener(listener -> {
+			//ready();
+			//playSound();
+			selectSong();
+		});
+		
+		
+		
+//		ssDialog.getEasy().addActionListener(e1 -> {
+//			file = "fml.wav";
+//		});
+//		ssDialog.getMedium().addActionListener(e2 -> {
+//			file = "Kalimba.wav";
+//		});
+//		ssDialog.getHard().addActionListener(e3 -> {
+//			file = "Sleep_Away.wav";
+//		});
+		
+		// Sollte in der Lage sein 2 noten zuerfassen Thread
 		view.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				t1 = new Thread(new Runnable() {
-					
+
 					@Override
 					public void run() {
-						if(e.getKeyCode() == KeyEvent.VK_D){
-						checkh0();				
-			    	}
-						
+						if (e.getKeyCode() == KeyEvent.VK_D) {
+							checkh0();
+						}
+
 					}
 				});
 				t2 = new Thread(new Runnable() {
@@ -84,92 +98,108 @@ public class MainController {
 						}
 
 					}
-				});			
+				});
 				t1.start();
 				t2.start();
 				t3.start();
 				t4.start();
 			}
-					
+
 		});
-		
+
 	}
-	private void ready(){
-		timer.start();	
+
+	private void ready() {
+		timer.start();
 		view.requestFocus();
 	}
-	private void update(){
+
+	private void update() {
 		time++;
 		color++;
 		list.checkTime(time);
 		list.moveNotes();
-		if(color==30){
+		if (color == 30) {
 			view.getPanel().setColor(Color.BLUE, 1);
 			view.getPanel().setColor(Color.BLUE, 2);
 			view.getPanel().setColor(Color.BLUE, 3);
 			view.getPanel().setColor(Color.BLUE, 4);
-			
+
 		}
 		view.updateView(list.getNotesList());
-		
-	}
-	private void checkh0(){
-		for(int i=0;i<list.getNotesList().size();i++){
-			if(hitbox.hit0(list.getNotesList().get(i).getNote())){
-    			score += 10;
-    			view.getPanel().setColor(Color.green, 1);
-    			color=0;
-    			list.remove(list.getNotesList().get(i));
-    			view.setScore(score);
-    		}
-		}
-	}
-	private void checkh1(){
-		for(int i=0;i<list.getNotesList().size();i++){
-			if(hitbox.hit1(list.getNotesList().get(i).getNote())){
-    			score += 10;
-    			view.getPanel().setColor(Color.green, 2);
-    			color=0;
-    			list.remove(list.getNotesList().get(i));
-    			view.setScore(score);
-    		}
-		}
-	}
-	private void checkh2(){
-		for(int i=0;i<list.getNotesList().size();i++){
-			if(hitbox.hit2(list.getNotesList().get(i).getNote())){
-    			score += 10;
-    			view.getPanel().setColor(Color.green, 3);
-    			color=0;
-    			list.remove(list.getNotesList().get(i));
-    			view.setScore(score);
-    		}
-		}
-	}
-	private void checkh3(){
-		for(int i=0;i<list.getNotesList().size();i++){
-			if(hitbox.hit3(list.getNotesList().get(i).getNote())){
-    			score += 10;
-    			view.getPanel().setColor(Color.green, 4);
-    			color=0;
-    			list.remove(list.getNotesList().get(i));
-    			view.setScore(score);
-    		}
-		}
-	}
-	public void playSound() {
-	    try {
-	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("fml.wav").getAbsoluteFile());
-	        Clip clip = AudioSystem.getClip();
-	        clip.open(audioInputStream);
-	        clip.start();
-	    } catch(Exception ex) {
-	        System.out.println("Error with playing sound.");
-	        ex.printStackTrace();
-	    }
-	}
-	public void selectSong(){
-		ssDialog = new SongSelectDialog("Choose a song", false);
-	}
-}
 
+	}
+
+	private void checkh0() {
+		for (int i = 0; i < list.getNotesList().size(); i++) {
+			if (hitbox.hit0(list.getNotesList().get(i).getNote())) {
+				score += 10;
+				view.getPanel().setColor(Color.green, 1);
+				color = 0;
+				list.remove(list.getNotesList().get(i));
+				view.setScore(score);
+			}
+		}
+	}
+
+	private void checkh1() {
+		for (int i = 0; i < list.getNotesList().size(); i++) {
+			if (hitbox.hit1(list.getNotesList().get(i).getNote())) {
+				score += 10;
+				view.getPanel().setColor(Color.green, 2);
+				color = 0;
+				list.remove(list.getNotesList().get(i));
+				view.setScore(score);
+			}
+		}
+	}
+
+	private void checkh2() {
+		for (int i = 0; i < list.getNotesList().size(); i++) {
+			if (hitbox.hit2(list.getNotesList().get(i).getNote())) {
+				score += 10;
+				view.getPanel().setColor(Color.green, 3);
+				color = 0;
+				list.remove(list.getNotesList().get(i));
+				view.setScore(score);
+			}
+		}
+	}
+
+	private void checkh3() {
+		for (int i = 0; i < list.getNotesList().size(); i++) {
+			if (hitbox.hit3(list.getNotesList().get(i).getNote())) {
+				score += 10;
+				view.getPanel().setColor(Color.green, 4);
+				color = 0;
+				list.remove(list.getNotesList().get(i));
+				view.setScore(score);
+			}
+		}
+	}
+
+	public void playSound() {
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(file).getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch (Exception ex) {
+			System.out.println("Error with playing sound.");
+			ex.printStackTrace();
+		}
+	}
+
+	public void selectSong() {
+		ssDialog = new SongSelectDialog(null, "Choose a song", true, this);
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public MyJFrame getView() {
+		return view;
+	}
+	
+}
--- RythmGame/src/game/SongSelectDialog.java
+++ RythmGame/src/game/SongSelectDialog.java
@@ -1,17 +1,73 @@
 package game;
 
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 
-public class SongSelectDialog extends JDialog{
-	
-	public SongSelectDialog(String title, boolean modal){
-		
-		JButton test = new JButton("hallo");
-		add(test);
-		
-		setSize(100, 100);
+public class SongSelectDialog extends JDialog {
+
+	private JButton easy = new JButton("Easy Song");
+	private JButton medium = new JButton("Medium Song");
+	private JButton hard = new JButton("Hard Song");
+
+	private JLabel easyLabel = new JLabel("Easy");
+	private JLabel mediumLabel = new JLabel("Medium");
+	private JLabel hardLabel = new JLabel("Hard");
+	private String songName;
+
+	private MainController controller;
+
+	public SongSelectDialog(Frame owner, String title, boolean modal, MainController controller) {
+		super(owner, title, modal);
+
+		this.controller = controller;
+
+		setLayout(new GridLayout(3, 1));
+
+		add(easyLabel);
+		add(easy);
+		add(mediumLabel);
+		add(medium);
+		add(hardLabel);
+		add(hard);
+
+		easy.addActionListener(new SongSelectActionListener(this, "fml.wav"));
+		medium.addActionListener(new SongSelectActionListener(this, "Kalimba.wav"));
+		hard.addActionListener(new SongSelectActionListener(this, "Sleep_Away.wav"));
+
+		pack();
 		setDefaultCloseOperation(HIDE_ON_CLOSE);
 		setVisible(true);
 	}
+
+	private class SongSelectActionListener implements ActionListener {
+
+		private SongSelectDialog dialog;
+
+		public SongSelectActionListener(SongSelectDialog dialog, String songName) {
+			this.dialog = dialog;
+			dialog.setSongName(songName);
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			controller.setFile(dialog.getSongName());
+			controller.playSound();
+			dialog.setVisible(false);
+			controller.getView().requestFocus();
+		}
+	}
+
+	public String getSongName() {
+		return songName;
+	}
+
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+
 }
